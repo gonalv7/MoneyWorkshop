9dd1c9f64bb4d7407cd66fda53a5c9ce41579f4c
alumnoFI <alumnoFI@FIW8LC0513.facultades.ort.edu.uy>
2015-10-29 19:11:40 -0300

    Se eliminan comentarios

--- src/test/java/ort/isagil/calculator/RunCukesTest.java
+++ src/test/java/ort/isagil/calculator/RunCukesTest.java
@@ -13,7 +13,5 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(strict=true, monochrome=true, format="junit:output", snippets=SnippetType.CAMELCASE, features="src/test/resources/ort/isagil/calculator/")
 public class RunCukesTest {
-    // this is the adapter/bridge code
-    // between cucumber jvm and junit
-    // you do not need to edit this
+    
 }
